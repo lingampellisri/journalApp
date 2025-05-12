e5c8b9034bf941d91ab154a89ed796cf7eda3559
Srinivas <lingampellisrinivas3036@gmail.com>
2025-05-12 17:42:20 +0530

    Simple RESTFULL API is Implemented

--- journalApp/src/main/java/net/engineeringdigest/journalApp/Controller/journelEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/Controller/journelEntryController.java
@@ -1,8 +1,31 @@
 package net.engineeringdigest.journalApp.Controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import net.engineeringdigest.journalApp.Entity.JournalEntry;
+import net.engineeringdigest.journalApp.JournalAppApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
+@RequestMapping("/journal")
 public class journelEntryController {
+
+    private  HashMap<Long, JournalEntry> JournalEntries=new HashMap<>();
+
+
+    @GetMapping()
+    public List<JournalEntry> getAll()
+    {
+        return new ArrayList<>(JournalEntries.values());
+    }
+    @PostMapping
+    public  boolean createEntry(@RequestBody JournalEntry myEntry)
+    {
+    JournalEntries.put(myEntry.getId(),myEntry);
+    return true;
+    }
+
 }
