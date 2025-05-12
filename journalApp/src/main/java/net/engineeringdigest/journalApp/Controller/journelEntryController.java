962305c26c3e06c45605c940934c7fd92f076de7
Srinivas <lingampellisrinivas3036@gmail.com>
2025-05-12 19:45:45 +0530

    Complete CRUD Operations Done

--- journalApp/src/main/java/net/engineeringdigest/journalApp/Controller/journelEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/Controller/journelEntryController.java
@@ -21,11 +21,37 @@ public class journelEntryController {
     {
         return new ArrayList<>(JournalEntries.values());
     }
-    @PostMapping
+    @PostMapping()
     public  boolean createEntry(@RequestBody JournalEntry myEntry)
     {
     JournalEntries.put(myEntry.getId(),myEntry);
     return true;
     }
+
+    @GetMapping("/id/{myId}")
+
+    public JournalEntry getJournalEntryById(@PathVariable Long myId)
+    {
+        return JournalEntries.get(myId);
+    }
+
+
+    @DeleteMapping("/id/{myId}")
+
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId)
+    {
+        return JournalEntries.remove(myId);
+    }
+
+
+
+    @PutMapping ("/id/{myId}")
+    public JournalEntry updateJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry )
+    {
+        return JournalEntries.put(myId,myEntry);
+    }
+
+
+
 
 }
