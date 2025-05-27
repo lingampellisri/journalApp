c447eacc4175d1efa3d060ef5490b21c71b4df2b
Srinivas <lingampellisrinivas3036@gmail.com>
2025-05-27 15:33:37 +0530

    lambok is added

--- journalApp/src/main/java/net/engineeringdigest/journalApp/Entity/JournalEntry.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/Entity/JournalEntry.java
@@ -1,6 +1,7 @@
 package net.engineeringdigest.journalApp.Entity;
 
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -17,37 +19,7 @@ public class JournalEntry {
     private  String content;
     private LocalDate date;
 
-    public LocalDate getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 
 
 }
