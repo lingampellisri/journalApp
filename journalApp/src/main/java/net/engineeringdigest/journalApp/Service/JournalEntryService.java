552b515d0f2ed71a12f35485ad1896cf1908fed8
Srinivas <lingampellisrinivas3036@gmail.com>
2025-05-29 11:41:33 +0530

    Transaction Control is Added

--- journalApp/src/main/java/net/engineeringdigest/journalApp/Service/JournalEntryService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/Service/JournalEntryService.java
@@ -7,6 +7,7 @@ import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -21,14 +22,21 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
-
+@Transactional
     public void saveEntry(JournalEntry journalEntry,String userName)
     {
-       User user=userService.findByUserName(userName);
-       journalEntry.setDate(LocalDate.now());
-       JournalEntry saved= journalEntryRepository.save(journalEntry);
-       user.getJournalEntries().add(saved);
-       userService.saveEntry(user);
+       try {
+           User user=userService.findByUserName(userName);
+           journalEntry.setDate(LocalDate.now());
+           JournalEntry saved= journalEntryRepository.save(journalEntry);
+           user.getJournalEntries().add(saved);
+           userService.saveEntry(user);
+       }
+       catch (Exception e)
+       {
+           System.out.println(e);
+           throw  new RuntimeException("Error Occured in SaveEntry Function",e);
+       }
     }
 
     public void saveEntry(JournalEntry journalEntry)
