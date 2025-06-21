fa39c6ed1a65d364d27b4ad6737d114e1134b127
Srinivas <lingampellisrinivas3036@gmail.com>
2025-06-21 13:28:34 +0530

    Mastering Logging in Spring Boot

--- journalApp/src/main/java/net/engineeringdigest/journalApp/Service/JournalEntryService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/Service/JournalEntryService.java
@@ -4,6 +4,8 @@ import net.engineeringdigest.journalApp.Entity.JournalEntry;
 import net.engineeringdigest.journalApp.Entity.User;
 import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +22,7 @@ public class JournalEntryService {
     private JournalEntryRepository journalEntryRepository;
     @Autowired
     private UserService userService;
+
 
 @Transactional
     public void saveEntry(JournalEntry journalEntry,String userName)
--- journalApp/src/main/java/net/engineeringdigest/journalApp/Service/UserService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/Service/UserService.java
@@ -1,8 +1,11 @@
 package net.engineeringdigest.journalApp.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.Entity.User;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +17,7 @@ import java.util.Optional;
 
 
 @Component
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -22,11 +26,39 @@ public class UserService {
     private static  final PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
 
 
+//    private static  final Logger logger= LoggerFactory.getLogger(UserService.class);
+//
+
     public void saveNewEntry(User user)
     {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-      UserRepository.save(user);
+       try{
+           user.setPassword(passwordEncoder.encode(user.getPassword()));
+           user.setRoles(Arrays.asList("USER"));
+           UserRepository.save(user);
+           log.error("Error occured for {} :",user.getUserName());
+           log.info("haaha INfo");
+           log.warn("haaha INfo");
+           log.debug("haaha INfo");
+           log.error("haaha INfo");
+
+
+       }
+       catch (Exception e)
+       {
+//           logger.info("haaha INfo");
+//           logger.warn("haaha INfo");
+//           logger.debug("haaha INfo");
+//           logger.error("haaha INfo");
+//           logger.trace("haaha INfo");
+
+            // it never runs this catch block  due to my own implementation of class
+
+           log.info("haaha INfo");
+           log.warn("haaha INfo");
+           log.debug("haaha INfo");
+           log.error("haaha INfo");
+           log.trace("haaha INfo");
+       }
     }
 
     public void saveAdminEntry(User user)
